7042987d0b9bd1d789721418795593293eaaba51
Peter Klipfel <peter@klipfel.me>
2013-09-22 15:22:35 -0600

    streaming audio

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
+var http = require('http'),
+    fileSystem = require('fs'),
+    path = require('path')
+    util = require('util');
 
 /*
  * GET home page.
@@ -7,6 +11,16 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
-exports.radio = function(req, res){
-  res.render('radio', { title: 'Robot Radio!' });
+exports.radio = function(req, res) {
+    var filePath = path.join(__dirname, '../uploads/6416-1e3cmpk.mp3');
+    var stat = fileSystem.statSync(filePath);
+    
+    res.writeHead(200, {
+        'Content-Type': 'audio/mpeg', 
+        'Content-Length': stat.size
+    });
+    
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to util.pump()
+    util.pump(readStream, res);
 };
